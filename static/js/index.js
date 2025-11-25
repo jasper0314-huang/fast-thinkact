c227bc79c2c0c59e26cf5bbba726b0eccbf1b603
jasper0314-huang <q0982569154@gmail.com>
2025-11-25 23:51:21 +0800

    Update

--- static/js/index.js
+++ static/js/index.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 	  autoplaySpeed: 10000,
 	};
 	var carousels = bulmaCarousel.attach(
-		'.carousel:not(#manipulation-carousel):not(#reflection-carousel)',
+		'.carousel:not(#manipulation-carousel):not(#reflection-carousel):not(#comparison-carousel)',
 		defaultOptions
 	  );
 
@@ -36,6 +36,16 @@ $(document).ready(function() {
 		autoplaySpeed: 20000, // you can adjust this if needed
 	  };
 	  var reflectionCarousel = bulmaCarousel.attach('#reflection-carousel', reflectionOptions);
+
+	// Comparison carousel (2x2 grid per page)
+	var comparisonOptions = {
+		slidesToScroll: 1,
+		slidesToShow: 1,
+		loop: true,
+		infinite: true,
+		autoplay: false, // Set to false so users can manually navigate
+	  };
+	  var comparisonCarousel = bulmaCarousel.attach('#comparison-carousel', comparisonOptions);
 
 	bulmaSlider.attach();
   });
